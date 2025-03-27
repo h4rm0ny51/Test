debf1a3fc3bf2a641506c0b01f80eecf9aeca4ea
Иван Кандауров <kandaurovv51@gmail.com>
2025-03-27 17:37:00 +0300

    Новая строка

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -11,7 +11,7 @@ int main()
     cout << "Получилось?";
     double x = 5;
     cout << "X =" << x;
-
+    cout << "Добавил строку";
     double y = 10;
     cout << "Y = " << y;
     cout << "Я добавил сюда Y ";
